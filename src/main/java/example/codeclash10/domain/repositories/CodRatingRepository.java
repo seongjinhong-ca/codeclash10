5dbd112b702bbac7141177b19c4cbf196edefa1c
seongjin <shawn95981@gmail.com>
2024-01-17 19:44:04 -0500

    Revert "created the repository for CodeRating entity"
    This reverts commit ffaf87ca01cccdb060509e504f9b1774ea8cb39c.

--- src/main/java/example/codeclash10/domain/repositories/CodRatingRepository.java
+++
@@ -1,7 +0,0 @@
-package example.codeclash10.domain.repositories;
-
-import example.codeclash10.domain.entities.CodeRating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CodRatingRepository extends JpaRepository<CodeRating, Integer> {
-}
